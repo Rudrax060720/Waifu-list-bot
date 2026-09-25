786f3b8c02a0003340b3291b35145a8872df1f11
Rudrax060720 <varshafultariya@gmail.com>
2026-09-26 01:06:36 +0530

    Update bot token and MongoDB URI
    Updated the bot token and MongoDB URI in Config.py.

---
+++ Config.py
@@ -0,0 +1,3 @@
+BOT_TOKEN = "8950401905:AAGf0wGcG5sywFNDmPBEMRdKN9eIfWUd0vU"
+MONGO_URI = "mongodb+srv://varshafultariya_db_user:Qlkfi44duIUh2UMd@cluster0.k3ej4ht.mongodb.net/?appName=Cluster0"
+DB_NAME = "waifu_system"
