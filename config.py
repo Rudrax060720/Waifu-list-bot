a8951ca97afd399b419ad8d7b50ea921edb3201d
Rudrax060720 <varshafultariya@gmail.com>
2026-09-26 01:16:43 +0530

    Add ADMINS list for Telegram user IDs
    Added a list of admin user IDs for Telegram.

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
 BOT_TOKEN = "8950401905:AAGf0wGcG5sywFNDmPBEMRdKN9eIfWUd0vU"
 MONGO_URI = "mongodb+srv://varshafultariya_db_user:Qlkfi44duIUh2UMd@cluster0.k3ej4ht.mongodb.net/?appName=Cluster0"
 DB_NAME = "waifu_system"
+# 👇 ADD THIS
+ADMINS = [8159874325]  # put your Telegram user IDs here
