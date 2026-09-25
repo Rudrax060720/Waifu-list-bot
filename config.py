f0c74daf9319fe6e2a976f188d0b5d0a46005c87
Rudrax060720 <varshafultariya@gmail.com>
2026-09-26 01:19:38 +0530

    Refactor config.py to use environment variables
    Refactor configuration to use environment variables for sensitive data and convert ADMINS from a string to a list of integers.

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
-BOT_TOKEN = "8950401905:AAGf0wGcG5sywFNDmPBEMRdKN9eIfWUd0vU"
-MONGO_URI = "mongodb+srv://varshafultariya_db_user:Qlkfi44duIUh2UMd@cluster0.k3ej4ht.mongodb.net/?appName=Cluster0"
-DB_NAME = "waifu_system"
-# 👇 ADD THIS
-ADMINS = [8159874325]  # put your Telegram user IDs here
+# put your Telegram user IDs here
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+MONGO_URI = os.getenv("MONGO_URI")
+DB_NAME = os.getenv("DB_NAME", "waifu_system")
+
+# Convert ENV string → list of ints
+ADMINS = list(map(int, os.getenv("ADMINS", "").split(",")))
